58ffd274b57b10e3b1bf9c0f7df5486ff1f36cad
David <nexfortisme@gmail.com>
2025-12-02 08:07:56 -0500

    Day 1 - Part 1

---
+++ Day 1/day1.ts
@@ -0,0 +1,103 @@
+
+const POS_MIN = 0;
+const POS_MAX = 99;
+
+const LEFT = -1;
+const RIGHT = 1;
+
+let pos = 50;
+let prevPos = 50;
+
+let zero_count = 0;
+
+
+let movementDelta: string | number = '';
+let movementDirection = 0;
+
+const textFile = await Deno.readTextFile("input.txt");
+
+let testBreak = 0;
+let lineCount = 1;
+
+for await (const textChar of textFile) {
+
+    prevPos = pos;
+
+    // End of Line - Process Movement
+    if( textChar == "\n" ) {
+
+        if(movementDirection == LEFT) { // Moving Left
+            console.log(`Movement Delta: ${movementDelta} on Line: ${lineCount} - Number Format: ${Number(movementDelta)} - PreMath: ${pos - Number(movementDelta)}`);
+            pos -= Number(movementDelta);
+            if (pos < POS_MIN) { // Crosses over zero
+
+                // pos += POS_MAX + 1; // Correcting for Fencepost
+                
+                // PART 2
+                // if (prevPos != 0){
+                //     console.log('Crossed Zero! *click*')
+                //     zero_count += 1;
+                // }
+            }
+        } else if(movementDirection == RIGHT) { // Moving Right
+            pos += Number(movementDelta);
+            if (pos > POS_MAX) { // Crosses over max
+                // pos -= POS_MAX + 1; // Correcting for Fencepost
+
+                // PART 2
+                // if (prevPos != 0){
+                //     console.log('Crossed Zero! *click*')
+                //     zero_count += 1;
+                // }
+            }
+        }
+
+        console.log(`Movement Direction: ${movementDirection == -1 ? 'Left' : 'Right'}, Movement Delta: ${movementDelta}, Previous Position: ${prevPos}, New Position: ${pos}. Zero Hits: ${zero_count}`);
+        
+        if (pos > POS_MAX) {
+            // console.log(`Something Fucked up in a spectacular way! On Line: ${lineCount}`);
+            while (pos > POS_MAX) {
+                pos -= (POS_MAX + 1);
+
+                // console.log('Overflow! Crossed Zero! *click*' + ' Current Pos: ' + pos)
+
+                // PART 2
+                // zero_count += 1;
+            }
+            // zero_count--; // Correcting for an over count
+        } else if (pos < POS_MIN) {
+            // console.log(`Something Fucked up in a spectacular way! On Line: ${lineCount}`);
+            while (pos < POS_MIN) {
+                pos += (POS_MAX + 1);
+
+                // console.log('Underflow! Crossed Zero! *click* Current Pos: ' + pos)
+
+                // // PART 2
+                // zero_count += 1;
+            }
+            // zero_count--; // Correcting for an over count
+        }
+
+        if(pos == 0 && prevPos != 0) {
+            console.log('Landed on Zero! *click*')
+            zero_count += 1;
+        }
+
+        movementDelta = '';
+        movementDirection = 0;
+        lineCount += 1;
+
+        continue;
+    }
+
+    if(textChar == "L") {
+        movementDirection = LEFT;
+    } else if(textChar == "R") {
+        movementDirection = RIGHT;
+    } else { // It's a number
+        movementDelta += textChar;
+    }
+}
+
+console.log(`Final Position: ${pos}`);
+console.log(`Total Zero Hits: ${zero_count}`);
